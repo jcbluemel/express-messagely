6b00388caf849b5db1fe76ffce5755b7c6c0e6b8
Joel <jcbluemel@yahoo.com>
2022-07-04 12:01:11 -0700

    working on users model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+
+
 /** User of the site. */
 
 class User {
@@ -9,6 +14,24 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+
+    //FIXME: can we use arguments[0] instead?
+    // const { username, password, first_name, last_name, phone } = {
+    //    username, password, first_name, last_name, phone }
+
+    const hashedPassword = await bcrypt.hash(
+      password, BCRYPT_WORK_FACTOR);
+
+    const user = await db.query(
+      `INSERT INTO users
+          (username, password, first_name, last_name, phone, join_at)
+        VALUES
+          ($1, $2, $3, $4, $5, NOW())
+        RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]
+    )
+
+    return user;
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
